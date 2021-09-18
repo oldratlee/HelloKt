e0da120cb5771169dd1e799bd655533635bb59c8
Jerry Lee <oldratlee@gmail.com>
2021-09-18 18:45:40 +0800

    add kotlin test libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+val kotestVersion = "4.6.3"
+
 plugins {
     kotlin("jvm") version "1.5.30"
     application
@@ -14,6 +16,9 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("io.kotest:kotest-property:$kotestVersion")
 }
 
 tasks.test {
