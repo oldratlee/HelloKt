5d7dbc87d90c20f20f7c8ab055ceb5034ed5e7b0
Jerry Lee <oldratlee@gmail.com>
2021-09-19 13:28:50 +0800

    add a playgroud demo: WeakHashMap Iteration when gc

--- README.md
+++ README.md
@@ -5,3 +5,16 @@ My Kotlin playground:
 - new kotlin version
 - Gradle Kts
 - kotest
+
+## Playground `WeakHashMap`: `ConcurrentModificationException` and `GC`
+
+when `WeakHashMap` key is `gc`ed in the `WeakHashMap` iteration,
+`WeakHashMap` iteration will throw `ConcurrentModificationException`?
+
+Demo Result: NO.
+
+Demo code: [`WeakHashMapGcIteration`](src/test/kotlin/playground/weakhashmap/WeakHashMapGcIteration.kt)
+
+```sh
+./gradlew execTestMain -P mainClass=playground.weakhashmap.WeakHashMapGcIterationKt
+```
---
+++ src/test/kotlin/playground/weakhashmap/WeakHashMapGcIteration.kt
@@ -0,0 +1,58 @@
+package playground.weakhashmap
+
+import java.util.*
+import kotlin.random.Random
+
+/**
+ * when [WeakHashMap] key is `gc`ed in the [WeakHashMap] iteration,
+ * `WeakHashMap` iteration will throw [ConcurrentModificationException]?
+ *
+ * Demo Result: NO.
+ */
+fun main() {
+    println(Key(271828)) // this Key 271828 will be GCed soon
+
+    val (keyList, weakHashMap) = prepareData()
+
+    for (round in (0..2)) {
+        val beginKeyListSize = keyList.size
+        val beginWeakHashMapSize = weakHashMap.size
+        println("[round $round] begin! keyListSize: $beginKeyListSize, weakHashMapSize: $beginWeakHashMapSize")
+
+        for (entry in weakHashMap.entries) {
+            check(entry.key.num == entry.value)
+
+            if (keyList.isEmpty()) {
+                println("keyList is empty, stop demo")
+                return
+            }
+            if (Random.nextDouble() >= 0.05) continue
+
+            val removed = keyList.removeAt(Random.nextInt(keyList.size))
+            println("[round $round] KeyList: removed $removed, keyListSize: ${keyList.size}, weakHashMapSize: ${weakHashMap.size}")
+            System.gc()
+        }
+        println("[round $round] end! keyListSize: $beginKeyListSize -> ${keyList.size}, weakHashMapSize: $beginWeakHashMapSize -> ${weakHashMap.size}")
+    }
+}
+
+private fun prepareData(): Pair<MutableList<Key>, WeakHashMap<Key, Int>> {
+    println("Preparing data...")
+
+    val keyList = (0 until 1000).map { Key(it) }.toMutableList()
+
+    // hold Keys in WeakHashMap
+    val weakHashMap = WeakHashMap<Key, Int>()
+    weakHashMap.putAll(keyList.associateWith { e -> e.num })
+
+    return Pair(keyList, weakHashMap)
+}
+
+
+private data class Key(val num: Int) {
+    // private val bigMem = (1..100).toList()
+
+    protected fun finalize() {
+        println("finalize $this")
+    }
+}
