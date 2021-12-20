fe9e0d4edb2d5373224335910c101b8b3c55a188
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-20 21:47:54 +0800

    Delete src directory

--- src/main/kotlin/kt/hello/Main.kt
+++
@@ -1,8 +0,0 @@
-package kt.hello
-
-fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments at Run/Debug configuration
-    println("Program arguments: ${args.joinToString()}")
-}
